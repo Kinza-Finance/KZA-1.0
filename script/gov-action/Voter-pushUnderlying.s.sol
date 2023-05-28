7c0cdb874beef852acb84642abdfe27c79157d26
personal <128370392+MuesliW@users.noreply.github.com>
2023-05-28 17:54:50 +0800

    clean up script naming, operational script

--- script/gov-action/Voter-pushUnderlying.s.sol
+++ script/gov-action/Voter-pushUnderlying.s.sol
@@ -8,18 +8,20 @@ contract VoterPushUnderlying is Script {
     function run() external {
         uint256 govPrivateKey = vm.envUint("PRIVATE_KEY_GOV");
         address voter = vm.envAddress("Voter");
-        string[] memory underlyings = new string[](5);
-        underlyings[0] = "underlying_USDC";
-        underlyings[1] = "underlying_WBTC";
-        underlyings[2] = "underlying_WETH";
-        underlyings[3] = "underlying_USDT";
-        underlyings[4] = "underlying_DAI";
+        string[] memory underlyings = new string[](6);
+        underlyings[0] = "UNDERLYING_BUSD";
+        underlyings[1] = "UNDERLYING_USDC";
+        underlyings[2] = "UNDERLYING_USDT";
+        underlyings[3] = "UNDERLYING_WETH";
+        underlyings[4] = "UNDERLYING_WBTC";
+        underlyings[5] = "UNDERLYING_WBNB";
         uint256 length = underlyings.length;
         vm.startBroadcast(govPrivateKey);
         for (uint i; i < length;++i) {
             address underlying = vm.envAddress(underlyings[i]);
-            Voter(voter).pushUnderlying(underlying);
-
+            if (i == 4 || i == 5) {
+                Voter(voter).pushUnderlying(underlying);
+            }
         }
         vm.stopBroadcast();
         
--- script/govSetUp.sh
+++ script/govSetUp.sh
@@ -1,12 +1,12 @@
 # !/bin/bash
 # this is sequential in the initial deployment, after all contract creation
-forge script script/gov-action/Minter-updateVoter.s.sol --rpc-url $MUMBAI_RPC_URL --broadcast -vvvv
-forge script script/gov-action/Minter-updateDistributor.s.sol --rpc-url $MUMBAI_RPC_URL --broadcast -vvvv
-forge script script/gov-action/XKZA-updateVoter.s.sol --rpc-url $MUMBAI_RPC_URL --broadcast -vvvv
-forge script script/gov-action/RewardsVault-updateTransferStrat.s.sol --rpc-url $MUMBAI_RPC_URL --broadcast -vvvv
-forge script script/gov-action/EDistributor-setVault.s.sol --rpc-url $MUMBAI_RPC_URL --broadcast -vvvv
-forge script script/gov-action/EDistributor-setEManager.s.sol --rpc-url $MUMBAI_RPC_URL --broadcast -vvvv
-forge script script/gov-action/EManager-setEmissionAdmin.s.sol --rpc-url $MUMBAI_RPC_URL --broadcast -vvvv
+forge script script/gov-action/Minter-updateVoter.s.sol --rpc-url $RPC_URL --broadcast -vvvv
+forge script script/gov-action/Minter-updateDistributor.s.sol --rpc-url $RPC_URL --broadcast -vvvv
+forge script script/gov-action/XKZA-updateVoter.s.sol --rpc-url $RPC_URL --broadcast -vvvv
+forge script script/gov-action/RewardsVault-updateTransferStrat.s.sol --rpc-url $RPC_URL --broadcast -vvvv
+forge script script/gov-action/EDistributor-setVault.s.sol --rpc-url $RPC_URL --broadcast -vvvv
+forge script script/gov-action/EDistributor-setEManager.s.sol --rpc-url $RPC_URL --broadcast -vvvv
+forge script script/gov-action/EManager-setEmissionAdmin.s.sol --rpc-url $RPC_URL --broadcast -vvvv
 
-forge script script/gov-action/KZA-initialMint.s.sol --rpc-url $MUMBAI_RPC_URL --broadcast -vvvv
-forge script script/gov-action/Voter-pushUnderlying.s.sol --rpc-url $MUMBAI_RPC_URL --broadcast -vvvv
+forge script script/gov-action/KZA-initialMint.s.sol --rpc-url $RPC_URL --broadcast -vvvv
+forge script script/gov-action/Voter-pushUnderlying.s.sol --rpc-url $RPC_URL --broadcast -vvvv
