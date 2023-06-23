0286556cb22945efbcf3f5ff1594912abb278132
personal <128370392+MuesliW@users.noreply.github.com>
2023-06-23 21:24:49 +0800

    add transferOwnership of Voter and Registry on script

--- script/11-transferOwnershipGOV.s.sol
+++ script/11-transferOwnershipGOV.s.sol
@@ -5,7 +5,9 @@ import "forge-std/Script.sol";
 import "../src/contracts/integration/LockTransferStrategy.sol";
 import "../src/contracts/KZA/KZA.sol";
 import "../src/contracts/KZA/Minter.sol";
+import "../src/contracts/KZA/Voter.sol";
 import "../src/contracts/KZA/XKZA.sol";
+import "../src/contracts/KZA/BribeAssetRegistry.sol";
 import "../src/contracts/integration/RewardsVault.sol";
 import "../src/contracts/KZA/KZADistributor.sol";
 //import "../src/contracts/lending/EmissionManager.sol";
@@ -18,7 +20,9 @@ contract TransferOwnership is Script {
         address kza = vm.envAddress("KZA");
         address minter = vm.envAddress("Minter");
         address xkza = vm.envAddress("XKZA");
+        address voter = vm.envAddress("Voter");
         address rv = vm.envAddress("RewardsVault");
+        address reg = vm.envAddresss("BribeAssetRegistry");
         address kdist = vm.envAddress("KZADistributor");
         //address em = vm.envAddress("EmissionManager");
         address ts = vm.envAddress("LockTransferStrategy");
@@ -26,10 +30,12 @@ contract TransferOwnership is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         KZA(kza).proposeNewGovernance(GOV);
+        Voter(voter).transferOwnership(GOV);
         Minter(minter).transferOwnership(GOV);
         XKZA(xkza).transferOwnership(GOV);
         RewardsVault(rv).transferOwnership(GOV);
         KZADistributor(kdist).transferOwnership(GOV);
+        BribeAssetRegistry(reg).transferOwnership(GOV);
         //EmissionManager(em).transferOwnership(GOV);
         LockTransferStrategy(ts).transferOwnership(GOV);
 
